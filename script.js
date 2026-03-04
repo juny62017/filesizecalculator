d980cb9ae55682ac67452b8a737f2191fddae85e
juny62017 <juny62017@gmail.com>
2026-03-05 01:51:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,127 @@
-var sizeInput = document.getElementById("sizeInput");
-var unitSelect = document.getElementById("unitSelect");
-var convertBtn = document.getElementById("convertBtn");
-var clearBtn = document.getElementById("clearBtn");
-var resultDiv = document.getElementById("result");
+var App = {
 
-convertBtn.onclick = function () {
+    elements: {
+        input: document.getElementById("sizeInput"),
+        unit: document.getElementById("unitSelect"),
+        convertBtn: document.getElementById("convertBtn"),
+        clearBtn: document.getElementById("clearBtn"),
+        result: document.getElementById("result")
+    },
 
-    var inputValue = sizeInput.value;
-    var selectedUnit = unitSelect.value;
+    init: function () {
+        this.attachEvents();
+    },
 
-    if (inputValue === "") {
-        resultDiv.innerText = "Please enter a value.";
-        return;
+    attachEvents: function () {
+        var self = this;
+
+        this.elements.convertBtn.onclick = function () {
+            self.handleConvert();
+        };
+
+        this.elements.clearBtn.onclick = function () {
+            self.handleClear();
+        };
+    },
+
+    handleConvert: function () {
+
+        var rawValue = this.elements.input.value;
+        var selectedUnit = this.elements.unit.value;
+
+        if (!this.validate(rawValue)) {
+            return;
+        }
+
+        var numericValue = Number(rawValue);
+
+        var convertedData = this.convertValues(numericValue, selectedUnit);
+
+        var formattedOutput = this.formatOutput(convertedData);
+
+        this.displayResult(formattedOutput);
+    },
+
+    handleClear: function () {
+        this.elements.input.value = "";
+        this.elements.result.innerHTML = "Output will show here";
+    },
+
+    validate: function (value) {
+
+        if (value === "") {
+            this.displayResult("Please enter a value.");
+            return false;
+        }
+
+        if (isNaN(value)) {
+            this.displayResult("Only numbers are allowed.");
+            return false;
+        }
+
+        if (Number(value) <= 0) {
+            this.displayResult("Value must be greater than 0.");
+            return false;
+        }
+
+        return true;
+    },
+
+    convertValues: function (value, unit) {
+
+        var kb = 0;
+        var mb = 0;
+        var gb = 0;
+
+        if (unit === "KB") {
+            kb = value;
+            mb = this.divideBy1024(value);
+            gb = this.divideBy1024(mb);
+        }
+
+        if (unit === "MB") {
+            kb = this.multiplyBy1024(value);
+            mb = value;
+            gb = this.divideBy1024(value);
+        }
+
+        if (unit === "GB") {
+            var tempMB = this.multiplyBy1024(value);
+            var tempKB = this.multiplyBy1024(tempMB);
+
+            kb = tempKB;
+            mb = tempMB;
+            gb = value;
+        }
+
+        return {
+            kb: kb,
+            mb: mb,
+            gb: gb
+        };
+    },
+
+    multiplyBy1024: function (num) {
+        return num * 1024;
+    },
+
+    divideBy1024: function (num) {
+        return num / 1024;
+    },
+
+    formatOutput: function (data) {
+
+        var line1 = "Kilobytes: " + data.kb.toFixed(2) + " KB";
+        var line2 = "Megabytes: " + data.mb.toFixed(2) + " MB";
+        var line3 = "Gigabytes: " + data.gb.toFixed(4) + " GB";
+
+        return line1 + "<br>" + line2 + "<br>" + line3;
+    },
+
+    displayResult: function (text) {
+        this.elements.result.innerHTML = text;
     }
 
-    if (isNaN(inputValue)) {
-        resultDiv.innerText = "Only numbers are allowed.";
-        return;
-    }
-
-    var numberValue = Number(inputValue);
-
-    if (numberValue <= 0) {
-        resultDiv.innerText = "Number must be greater than 0.";
-        return;
-    }
-
-    var valueInKB = 0;
-    var valueInMB = 0;
-    var valueInGB = 0;
-
-    if (selectedUnit === "KB") {
-
-        valueInKB = numberValue;
-
-        var tempMB = numberValue / 1024;
-        valueInMB = tempMB;
-
-        var tempGB = tempMB / 1024;
-        valueInGB = tempGB;
-    }
-
-    else if (selectedUnit === "MB") {
-
-        var tempKB = numberValue * 1024;
-        valueInKB = tempKB;
-
-        valueInMB = numberValue;
-
-        var tempGB2 = numberValue / 1024;
-        valueInGB = tempGB2;
-    }
-
-    else if (selectedUnit === "GB") {
-
-        var tempMB3 = numberValue * 1024;
-        valueInMB = tempMB3;
-
-        var tempKB2 = tempMB3 * 1024;
-        valueInKB = tempKB2;
-
-        valueInGB = numberValue;
-    }
-
-    var formattedKB = valueInKB.toFixed(2);
-    var formattedMB = valueInMB.toFixed(2);
-    var formattedGB = valueInGB.toFixed(4);
-
-    var outputText = "";
-    outputText += "Kilobytes: " + formattedKB + " KB <br>";
-    outputText += "Megabytes: " + formattedMB + " MB <br>";
-    outputText += "Gigabytes: " + formattedGB + " GB";
-
-    resultDiv.innerHTML = outputText;
 };
 
-clearBtn.onclick = function () {
-
-    sizeInput.value = "";
-
-    resultDiv.innerText = "Output will show here";
-
-};
+App.init();
